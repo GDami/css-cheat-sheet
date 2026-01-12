348817cd89fa361a669d15bf7b0b48560c6af82c
Damien <d.gervy@free.fr>
2026-01-12 13:58:52 +0100

    header style

--- app/layout/Header.tsx
+++ app/layout/Header.tsx
@@ -1,13 +1,17 @@
 import { PropsWithChildren } from "react";
 
+const styles = {
+    title: "text-3xl font-bold mb-2",
+}
+
 export default function Header({ children }: PropsWithChildren) {
     return (
-        <header>
-            <div>
-                <h1>CSS Playground</h1>
+        <header className="border">
+            <div className="border flex flex-col items-center justify-center m-4">
+                <h1 className={styles.title}>CSS Playground</h1>
                 <p>CSS Cheat Sheet with visual examples !</p>
             </div>
-            <div>
+            <div className="border">
                 { children }
             </div>
         </header>
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,44 @@
 import VisualExample from "./components/VisualExample";
 import Header from "./layout/Header";
 
+const styles = {
+    exampleBox: "rounded-lg",
+}
+
 export default function Home() {
-  return (
-    <div>
-        <Header></Header>
-        <main>
-            <section id="Spacing">
-                <div className="grid grid-cols-2 gap-4">
-                    <VisualExample title="Margin" description="The margin property sets the margin area on all four sides of an element.">
-                        <div className="bg-blue-500 m-8 p-4 text-white">
-                            This box has a margin of 8 units.
-                        </div>
-                    </VisualExample>
-                    <VisualExample title="Padding" description="The padding property sets the padding area on all four sides of an element.">
-                        <div className="bg-green-500 p-8 text-white">
-                            This box has a padding of 8 units.
-                        </div>
-                    </VisualExample>
-                </div>
-            </section>
-        </main>
-    </div>
-  );
+    return (
+        <div>
+            <Header></Header>
+            <main>
+                <section id="Spacing" className="mt-8">
+                    <div className="grid grid-cols-2 gap-4">
+                        <VisualExample title="Margin" description="The margin property sets the margin area on all four sides of an element.">
+                            <div className={` ${styles.exampleBox} bg-blue-500 m-8 p-4 text-white`}>
+                                This box has a margin of 32 pixels.
+                            </div>
+                        </VisualExample>
+                        <VisualExample title="Padding" description="The padding property sets the padding area on all four sides of an element.">
+                            <div className={` ${styles.exampleBox} bg-green-500 p-8 text-white`}>
+                                This box has a padding of 32 pixels.
+                            </div>
+                        </VisualExample>
+                    </div>
+                </section>
+                <section id="Typography" className="mt-8">
+                    <div className="grid grid-cols-2 gap-4">
+                        <VisualExample title="Font Size" description="The font-size property sets the size of the font.">
+                            <div className={` ${styles.exampleBox} bg-red-500 text-white text-xl p-4`}>
+                                This text has a font size of 1.25rem.
+                            </div>
+                        </VisualExample>
+                        <VisualExample title="Line Height" description="The line-height property sets the height of a line box.">
+                            <div className={` ${styles.exampleBox} bg-purple-500 text-white leading-1.5 p-4`}>
+                                This text has a line height of 1.5.
+                            </div>
+                        </VisualExample>
+                    </div>
+                </section>
+            </main>
+        </div>
+    );
 }
