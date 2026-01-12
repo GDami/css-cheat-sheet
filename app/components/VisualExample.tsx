3dbaa4ece59cc6acf3f18c456b85f1b0aaee7d00
GDami <41579255+GDami@users.noreply.github.com>
2026-01-12 22:50:09 +0100

    style

--- app/components/VisualExample.tsx
+++ app/components/VisualExample.tsx
@@ -1,18 +1,30 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, ReactNode } from "react";
 
-type VisualExampleProps = PropsWithChildren & {
-    title: string;
-    description: string;
+const inlineColors = {
+    blue: "bg-blue-50 border-blue-200 hover:border-blue-300",
+    red: "bg-red-50 border-red-200 hover:border-red-300",
 }
 
-export default function VisualExample({ title, description, children }: VisualExampleProps) {
+type VisualExampleProps = PropsWithChildren & {
+    title: string,
+    description: string,
+    code: string,
+    color: keyof typeof inlineColors,
+}
+
+export default function VisualExample({ title, description, children, code, color = "blue" }: VisualExampleProps) {
     return (
-        <div>
-            <h2 className="text-xl font-bold mb-2">{title}</h2>
-            <p className="mb-4">{description}</p>
-            <div className="border p-4 bg-gray-100">
+        <div className={`border-paper-solid hover:border-red hover:shadow-lg p-6 border-2 flex flex-col gap-4 rounded-2xl transition-all`}>
+            <div>
+                <h3 className="text-lg">{title}</h3>
+                <span className="text-sm text-gray-600">{description}</span>
+            </div>
+            <div className="border-2 border-dashed border-gray-200 bg-white rounded-xl p-4">
                 {children}
             </div>
+            <pre className="bg-gray-900/90 text-white p-4 rounded-xl text-sm">
+                <code>{code}</code>
+            </pre>
         </div>
     )
 }
--- app/layout/Header.tsx
+++ app/layout/Header.tsx
@@ -6,12 +6,12 @@ const styles = {
 
 export default function Header({ children }: PropsWithChildren) {
     return (
-        <header className="border">
-            <div className="border flex flex-col items-center justify-center m-4">
+        <header className="sticky top-0 border-b-20 border-paper-solid bg-white/80 backdrop-blur-xs">
+            <div className="flex flex-col items-center justify-center m-4">
                 <h1 className={styles.title}>CSS Playground</h1>
                 <p>CSS Cheat Sheet with visual examples !</p>
             </div>
-            <div className="border">
+            <div>
                 { children }
             </div>
         </header>
