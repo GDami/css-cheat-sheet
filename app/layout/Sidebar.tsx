48a250f1462eba1298155ffe92326f2821958008
GDami <41579255+GDami@users.noreply.github.com>
2026-01-16 12:34:40 +0100

    header + sidebar layout next : make sidebar responsive and connect it

---
+++ app/layout/Sidebar.tsx
@@ -0,0 +1,39 @@
+export default function Sidebar() {
+    return (
+        <aside className="menus sticky top-0 left-0 w-xs border-r-8 p-4 overflow-auto">
+            <nav className="flex flex-col gap-4">
+                <a href="#introduction" className="text-gray-700 hover:text-black">Introduction</a>
+                <a href="#getting-started" className="text-gray-700 hover:text-black">Getting Started</a>
+                <a href="#components" className="text-gray-700 hover:text-black">Components</a>
+                <a href="#introduction" className="text-gray-700 hover:text-black">Introduction</a>
+                <a href="#getting-started" className="text-gray-700 hover:text-black">Getting Started</a>
+                <a href="#components" className="text-gray-700 hover:text-black">Components</a>
+                <a href="#introduction" className="text-gray-700 hover:text-black">Introduction</a>
+                <a href="#getting-started" className="text-gray-700 hover:text-black">Getting Started</a>
+                <a href="#components" className="text-gray-700 hover:text-black">Components</a>
+                <a href="#introduction" className="text-gray-700 hover:text-black">Introduction</a>
+                <a href="#getting-started" className="text-gray-700 hover:text-black">Getting Started</a>
+                <a href="#components" className="text-gray-700 hover:text-black">Components</a>
+                <a href="#introduction" className="text-gray-700 hover:text-black">Introduction</a>
+                <a href="#getting-started" className="text-gray-700 hover:text-black">Getting Started</a>
+                <a href="#components" className="text-gray-700 hover:text-black">Components</a>
+                <a href="#introduction" className="text-gray-700 hover:text-black">Introduction</a>
+                <a href="#getting-started" className="text-gray-700 hover:text-black">Getting Started</a>
+                <a href="#components" className="text-gray-700 hover:text-black">Components</a>
+                <a href="#introduction" className="text-gray-700 hover:text-black">Introduction</a>
+                <a href="#getting-started" className="text-gray-700 hover:text-black">Getting Started</a>
+                <a href="#components" className="text-gray-700 hover:text-black">Components</a>
+                <a href="#introduction" className="text-gray-700 hover:text-black">Introduction</a>
+                <a href="#getting-started" className="text-gray-700 hover:text-black">Getting Started</a>
+                <a href="#components" className="text-gray-700 hover:text-black">Components</a>
+                <a href="#introduction" className="text-gray-700 hover:text-black">Introduction</a>
+                <a href="#getting-started" className="text-gray-700 hover:text-black">Getting Started</a>
+                <a href="#components" className="text-gray-700 hover:text-black">Components</a>
+                <a href="#introduction" className="text-gray-700 hover:text-black">Introduction</a>
+                <a href="#getting-started" className="text-gray-700 hover:text-black">Getting Started</a>
+                <a href="#components" className="text-gray-700 hover:text-black">Components</a>
+                <a href="#introduction" className="text-gray-700 hover:text-black">Introduction</a>
+            </nav>
+        </aside>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,122 +1,120 @@
 import VisualExample from "./components/VisualExample";
 import Header from "./layout/Header";
+import Sidebar from "./layout/Sidebar";
 
 import "./styles/page.css";
 
-
-
-const inlineStyles = {
-    exampleBox: "rounded-lg",
-    section: "flex flex-col gap-8",
-    sectionGrid: "grid grid-cols-2 gap-8",
-}
-
 export default function Home() {
     return (
-        <div className="wood bg-paper text-wood">
-            <div className="flex flex-col items-center">
-                <Header></Header>
-                <div className="z-1 relative mb-20 shadow-2xl shadow-black">
-                    <div className="undersheet shifted-3 rounded-xl shadow-lg shadow-black"></div>
-                    <div className="undersheet shifted-2 rounded-xl shadow-lg shadow-black"></div>
-                    <div className="undersheet shifted-1 rounded-xl shadow-lg shadow-black"></div>
-                    <main className="paper rounded-xl max-w-5xl text-gray-900 px-8 border-paper-solid flex flex-col gap-8 py-8 shadow-lg shadow-black">
-                        <section id="Spacing" className={inlineStyles.section}>
-                            <h2>Spacing</h2>
-                            {/* TODO : right/left/x/y */}
-                            <div className={inlineStyles.sectionGrid}>
-                                <VisualExample
-                                    title="Margin"
-                                    description="The margin property sets the margin area on all four sides of an element."
-                                    code={`margin: 32px;`}
-                                    color="blue">
-                                    <div className={` ${inlineStyles.exampleBox} bg-blue-500 m-[32px] text-white`}>
-                                        This box has a margin of 32 pixels.
+        <main className="wood flex flex-col">
+            <Header></Header>
+            <div className="flex h-[calc(100dvh-24px-var(--text-2xl)*var(--text-2xl--line-height))]">
+                <Sidebar></Sidebar>
+                <div className="wood w-full bg-paper text-wood overflow-auto">
+                    <div className="flex justify-center">
+                        <div className="z-1 relative m-8 w-full shadow-2xl shadow-black">
+                            <div className="undersheet shifted-3 rounded-xl shadow-lg shadow-black"></div>
+                            <div className="undersheet shifted-2 rounded-xl shadow-lg shadow-black"></div>
+                            <div className="undersheet shifted-1 rounded-xl shadow-lg shadow-black"></div>
+                            <div className="paper rounded-xl text-gray-900 px-8 border-paper-solid flex flex-col gap-8 py-8 shadow-lg shadow-black">
+                                <section id="Spacing" className="flex flex-col gap-8">
+                                    <h2>Spacing</h2>
+                                    {/* TODO : right/left/x/y */}
+                                    <div className="grid grid-cols-2 gap-8">
+                                        <VisualExample
+                                            title="Margin"
+                                            description="The margin property sets the margin area on all four sides of an element."
+                                            code={`margin: 32px;`}
+                                            color="blue">
+                                            <div className="rounded-lg bg-blue-500 m-[32px] text-white">
+                                                This box has a margin of 32 pixels.
+                                            </div>
+                                        </VisualExample>
+                                        <VisualExample
+                                            title="Padding"
+                                            description="The padding property sets the padding area on all four sides of an element."
+                                            code={`padding: 32px;`}
+                                            color="blue">
+                                            <div className="rounded-lg bg-green-500 p-[32px] text-white">
+                                                This box has a padding of 32 pixels.
+                                            </div>
+                                        </VisualExample>
                                     </div>
-                                </VisualExample>
-                                <VisualExample
-                                    title="Padding"
-                                    description="The padding property sets the padding area on all four sides of an element."
-                                    code={`padding: 32px;`}
-                                    color="blue">
-                                    <div className={` ${inlineStyles.exampleBox} bg-green-500 p-[32px] text-white`}>
-                                        This box has a padding of 32 pixels.
+                                </section>
+                                <section id="Typography" className="flex flex-col gap-8">
+                                    <h2>Typography</h2>
+                                    <div className="grid grid-cols-2 gap-8">
+                                        <VisualExample
+                                            title="Font Size"
+                                            description="The font-size property sets the size of the font."
+                                            code={`font-size: 1.25rem;`}
+                                            color="red">
+                                            <div className={"rounded-lg bg-red-500 text-white text-[1.25rem] p-4"}>
+                                                This text has a font size of 1.25rem.
+                                            </div>
+                                        </VisualExample>
+                                        <VisualExample
+                                            title="Line Height"
+                                            description="The line-height property sets the height of a line box."
+                                            code={`line-height: 1.5;`}
+                                            color="red">
+                                            <div className="rounded-lg bg-purple-500 text-white leading-[1.5] p-4">
+                                                This text has a line height of 1.5.
+                                            </div>
+                                        </VisualExample>
                                     </div>
-                                </VisualExample>
-                            </div>
-                        </section>
-                        <section id="Typography" className={inlineStyles.section}>
-                            <h2>Typography</h2>
-                            <div className={inlineStyles.sectionGrid}>
-                                <VisualExample
-                                    title="Font Size"
-                                    description="The font-size property sets the size of the font."
-                                    code={`font-size: 1.25rem;`}
-                                    color="red">
-                                    <div className={` ${inlineStyles.exampleBox} bg-red-500 text-white text-[1.25rem] p-4`}>
-                                        This text has a font size of 1.25rem.
-                                    </div>
-                                </VisualExample>
-                                <VisualExample
-                                    title="Line Height"
-                                    description="The line-height property sets the height of a line box."
-                                    code={`line-height: 1.5;`}
-                                    color="red">
-                                    <div className={` ${inlineStyles.exampleBox} bg-purple-500 text-white leading-[1.5] p-4`}>
-                                        This text has a line height of 1.5.
-                                    </div>
-                                </VisualExample>
-                            </div>
-                        </section>
-                        <section id="Typography" className={inlineStyles.section}>
-                            <h2>Typography</h2>
-                            <div className={inlineStyles.sectionGrid}>
-                                <VisualExample
-                                    title="Font Size"
-                                    description="The font-size property sets the size of the font."
-                                    code={`font-size: 1.25rem;`}
-                                    color="red">
-                                    <div className={` ${inlineStyles.exampleBox} bg-red-500 text-white text-[1.25rem] p-4`}>
-                                        This text has a font size of 1.25rem.
-                                    </div>
-                                </VisualExample>
-                                <VisualExample
-                                    title="Line Height"
-                                    description="The line-height property sets the height of a line box."
-                                    code={`line-height: 1.5;`}
-                                    color="red">
-                                <div className={` ${inlineStyles.exampleBox} bg-purple-500 text-white leading-[1.5] p-4`}>
-                                    This text has a line height of 1.5.
+                                </section>
+                                <section id="Typography" className="flex flex-col gap-8">
+                                    <h2>Typography</h2>
+                                    <div className="grid grid-cols-2 gap-8">
+                                        <VisualExample
+                                            title="Font Size"
+                                            description="The font-size property sets the size of the font."
+                                            code={`font-size: 1.25rem;`}
+                                            color="red">
+                                            <div className="rounded-lg bg-red-500 text-white text-[1.25rem] p-4">
+                                                This text has a font size of 1.25rem.
+                                            </div>
+                                        </VisualExample>
+                                        <VisualExample
+                                            title="Line Height"
+                                            description="The line-height property sets the height of a line box."
+                                            code={`line-height: 1.5;`}
+                                            color="red">
+                                        <div className="rounded-lg bg-purple-500 text-white leading-[1.5] p-4">
+                                            This text has a line height of 1.5.
+                                        </div>
+                                    </VisualExample>
                                 </div>
-                            </VisualExample>
+                            </section>
+                            <section id="Typography" className="flex flex-col gap-8">
+                                <h2>Typography</h2>
+                                <div className="grid grid-cols-2 gap-8">
+                                    <VisualExample
+                                        title="Font Size"
+                                        description="The font-size property sets the size of the font."
+                                        code={`font-size: 1.25rem;`}
+                                        color="red">
+                                        <div className="rounded-lg bg-red-500 text-white text-[1.25rem] p-4">
+                                            This text has a font size of 1.25rem.
+                                        </div>
+                                    </VisualExample>
+                                    <VisualExample
+                                        title="Line Height"
+                                        description="The line-height property sets the height of a line box."
+                                        code={`line-height: 1.5;`}
+                                        color="red">
+                                        <div className="rounded-lg bg-purple-500 text-white leading-[1.5] p-4">
+                                            This text has a line height of 1.5.
+                                        </div>
+                                    </VisualExample>
+                                </div>
+                            </section>
                         </div>
-                    </section>
-                    <section id="Typography" className={inlineStyles.section}>
-                        <h2>Typography</h2>
-                        <div className={inlineStyles.sectionGrid}>
-                            <VisualExample
-                                title="Font Size"
-                                description="The font-size property sets the size of the font."
-                                code={`font-size: 1.25rem;`}
-                                color="red">
-                                <div className={` ${inlineStyles.exampleBox} bg-red-500 text-white text-[1.25rem] p-4`}>
-                                    This text has a font size of 1.25rem.
-                                </div>
-                            </VisualExample>
-                            <VisualExample
-                                title="Line Height"
-                                description="The line-height property sets the height of a line box."
-                                code={`line-height: 1.5;`}
-                                color="red">
-                                <div className={` ${inlineStyles.exampleBox} bg-purple-500 text-white leading-[1.5] p-4`}>
-                                    This text has a line height of 1.5.
-                                </div>
-                            </VisualExample>
                         </div>
-                    </section>
-                </main>
+                    </div>
                 </div>
             </div>
-        </div>
+        </main>
     );
 }
